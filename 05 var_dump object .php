fc245a964e61167259299d21831e050f8360d219
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-29 15:50:55 +0700

    05 var_dump object

---
+++ 05 var_dump object .php
@@ -0,0 +1,18 @@
+<?php
+class Mobil {
+    public $merk;
+    public $model;
+}
+echo '<pre>';
+
+$mobil_baru = new Mobil();
+$mobil_baru->merk = "Pajero";
+$mobil_baru->model = "Fortuner";
+
+var_dump($mobil_baru);
+
+print_r("<p><strong><i>By Meylisa eka putry</strong>");
+
+echo '</pre>';
+
+?>
