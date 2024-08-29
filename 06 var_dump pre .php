232d33eb86ff6ade9509a31f2e92bfffc8e3258b
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-29 16:02:14 +0700

    06 var_dump pre

---
+++ 06 var_dump pre .php
@@ -0,0 +1,9 @@
+<?php
+$variabel_array = array("Teh", "Kopi", "Jus apel");
+echo "<pre>";
+var_dump($variabel_array);
+echo "</pre>";
+
+print_r("<p><strong><i>By Meylisa eka putry</strong>");
+
+?>
