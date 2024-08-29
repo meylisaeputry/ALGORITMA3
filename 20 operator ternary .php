f639bf0d4dad850a909df094f058a0c4e83c3c1d
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-29 20:42:57 +0700

    20 operator trnary php

---
+++ 20 operator ternary .php
@@ -0,0 +1,11 @@
+<?php
+
+$suka = true;
+
+// menggunakan operator ternary
+$jawab = $suka ? "iya": "tidak";
+
+// menampilkan jawaban
+echo $jawab;
+
+print_r("<p><strong><i>By Meylisa eka putry</strong>");
