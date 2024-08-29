fe5f2daa02a6068ae8fff17d29c30940b1822dd4
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-29 19:17:41 +0700

    09 die php

---
+++ 09 die .php
@@ -0,0 +1,27 @@
+<?php
+// Penggunaan Dasar fungsi die()
+if (file_exists('file_important.txt')) {
+    echo "File ditemukan.";
+} else {
+    die("Error: File tidak ditemukan, dan logika selanjutnya tidak akan dieksekusi.");
+
+}
+// Menambahkan Status HTTP
+if (!user_is_logged_in()) {
+    header('HTTP/1.1 403 Forbidden');
+    die("Error: Kamu tidak memiliki akses.");
+}
+// Menggabungkan dengan fungsi exit()
+if ($koneksi_database == false) {
+    exit(1); // menghentikan skrip dengan kode keluar 1
+}
+// Memahami Output Buffering
+ob_start();
+echo "Ini adalah output buffer.";
+
+// Kondisi tertentu
+if ($error) {
+    ob_end_clean(); // bersihkan output buffer
+    die("Skrip dihentikan karena error.");
+
+}
---
+++ 10 print r array .php
@@ -0,0 +1,23 @@
+
+<?php
+$array = array(
+    'name' => 'John Doe',
+    'age' => 30,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+print_r($array);
+
+print "<p>";
+
+$array = array(
+    'name' => 'John Doe',
+    'age' => 30,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+var_dump($array);
+
+print "<p>";
+
+print_r("<p><strong><i>By Meylisa eka putry</strong>");
