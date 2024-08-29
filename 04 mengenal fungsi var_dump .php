f12ce84508ec6a16e80f5875eb8432a61c402c07
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-29 15:43:43 +0700

    04 mengenal fungsi var_dump .php

---
+++ 04 mengenal fungsi var_dump .php
@@ -0,0 +1,17 @@
+
+<?php
+
+$variabel_string = "Halo Dunia tipu-tipu!";
+echo '<pre>';
+$variabel_integer = 123;
+$variabel_array = array(1, 2, "tiga", 4.5);
+
+var_dump($variabel_string);
+var_dump($variabel_integer);
+var_dump($variabel_array);
+
+print_r("<p><strong><i>By Meylisa eka putry</strong>");
+
+echo '</pre>'
+
+?>
