819bcc3d188da3dea1827b3a57eac0ba98911ede
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-29 15:13:51 +0700

    01 print r

---
+++ print r .php
@@ -0,0 +1,9 @@
+<?php
+$nama = "Meylisa";
+print_r($nama);
+print_r ("<p>");
+$Minuman = array("Teh", "Kopi", "Jus apel");
+print_r($Minuman);
+
+print_r("<p><strong><i>By Meylisa eka putry</strong>");
+?>
