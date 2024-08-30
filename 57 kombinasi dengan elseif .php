0bff9f6fbbf55b5e9d1e68c9efbe96c6a71e5467
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-30 10:17:53 +0700

    57 kombinasi dengan nilai elseif php

---
+++ 57 kombinasi dengan elseif .php
@@ -0,0 +1,12 @@
+<?php
+$suhu = 30;
+
+if ($suhu < 20) {
+    echo "Cuaca dingin.";
+} elseif ($suhu <= 30) {
+    echo "Cuaca hangat.";
+} else {
+    echo "Cuaca panas.";
+}
+
+print_r("<p><strong><i>By Meylisa eka putry</strong>");
