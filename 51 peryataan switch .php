a9d402fc9e3078a02dbccab3e7049e875ae8c59d
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-30 09:55:44 +0700

    51 peryataan switch

---
+++ 51 peryataan switch .php
@@ -0,0 +1,15 @@
+<?php
+$grade = 'B';
+switch ($grade) {
+  case 'A':
+    echo "Luar biasa!";
+    break;
+  case 'B':
+  case 'C':
+    echo "Bagus!";
+    break;
+  default:
+    echo "Tetap semangat!";
+}
+
+print_r("<p><strong><i>By Meylisa eka putry</strong>");
