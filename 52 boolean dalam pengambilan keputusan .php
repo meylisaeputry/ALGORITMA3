314c55d87fcd7bac2ff4e26813474fb9a1441eeb
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-30 09:58:38 +0700

    52 boolean pengambilan keputusan

---
+++ 52 boolean dalam pengambilan keputusan .php
@@ -0,0 +1,10 @@
+<?php
+$login_sukses = true;
+
+if ($login_sukses) {
+    echo "Selamat datang pengguna!";
+} else {
+    echo "Login gagal, silakan coba lagi.";
+}
+
+print_r("<p><strong><i>By Meylisa eka putry</strong>");
